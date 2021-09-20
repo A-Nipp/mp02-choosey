2ecc48b4d12509564b815ca55649c340f6d58bf2
samrshi <samrshi@outlook.com>
2021-09-20 00:32:37 -0400

    Clean up error handling and indentation

--- p01-choosey/Service/YelpService.swift
+++ p01-choosey/Service/YelpService.swift
@@ -8,47 +8,46 @@
 import Foundation
 
 enum NetworkError: String, Error {
-  case invalidUrl = "Invalid URL."
-  case failedRequest = "The network request failed."
-  case missingData = "Did not receive expected data."
+    case invalidUrl = "Invalid URL."
+    case failedRequest = "The network request failed."
+    case missingData = "Did not receive expected data."
 }
 
-class YelpService {
-  static func getBusinesses(term: String, radius: Int, completion: @escaping (Result<[Business], NetworkError>) -> Void) {
-    let baseUrl = "https://api.yelp.com/v3/businesses/search"
-    var components = URLComponents(string: baseUrl)
-    
-    components?.queryItems = [
-      URLQueryItem(name: "latitude", value: "35.913200"),
-      URLQueryItem(name: "longitude", value: "-79.055847"),
-      URLQueryItem(name: "term", value: term),
-      URLQueryItem(name: "radius", value: "\(radius)"),
-    ]
-    
-    guard let url = components?.url else {
-      completion(.failure(.invalidUrl))
-      return
-    }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = "GET"
-    request.setValue("Bearer \(Secrets.yelpApiKey)", forHTTPHeaderField: "Authorization")
-    
-    URLSession.shared.dataTask(with: request) { data, response, error in
-      if error != nil {
-        completion(.failure(.failedRequest))
-      }
-      
-        guard let data = data else { return }
-        
-        do {
-            let response = try JSONDecoder().decode(Response.self, from: data)
-            completion(.success(response.businesses))
-        } catch {
-            print(String(describing: error))
-            completion(.failure(.missingData))
-       }
+enum YelpService {
+    static func getBusinesses(term: String, radius: Int, completion: @escaping (Result<[Business], NetworkError>) -> Void) {
+        let baseUrl = "https://api.yelp.com/v3/businesses/search"
+        var components = URLComponents(string: baseUrl)
 
-    }.resume()
-  }
+        components?.queryItems = [
+            URLQueryItem(name: "latitude", value: "35.913200"),
+            URLQueryItem(name: "longitude", value: "-79.055847"),
+            URLQueryItem(name: "term", value: term),
+            URLQueryItem(name: "radius", value: "\(radius)"),
+        ]
+
+        guard let url = components?.url else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(Secrets.yelpApiKey)", forHTTPHeaderField: "Authorization")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil, let data = data else {
+                completion(.failure(.failedRequest))
+                return
+            }
+
+            do {
+                let response = try JSONDecoder().decode(Response.self, from: data)
+                completion(.success(response.businesses))
+            } catch {
+                print(String(describing: error))
+                completion(.failure(.missingData))
+            }
+
+        }.resume()
+    }
 }
